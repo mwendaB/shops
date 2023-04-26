abb911e02e4b76e3fb5c68d80418837488bf869a
mwendaB <brianmwenda255@gmail.com>
2023-04-26 23:39:49 +0300

    create page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,25 @@
-'use client'
+"use client";
 import CityPicker from "@/components/CityPicker";
 import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
   return (
-  <div className="min-h-screen bg-gradient-to-br from-[#394f68] to-[#18387e]
-    p-10 flex flex-col justify-center items-center">
-    <Card className="max-w-4xl mx-auto">
-    <Text className="text-6xl font-bold text-center mb-10">
-      Weather App
-    </Text>
-    <Subtitle className="text-xl text-center">
-      Powered by OpenAi
-    </Subtitle>
+    <div
+      className="min-h-screen bg-gradient-to-br from-[#394f68] to-[#18387e]
+    p-10 flex flex-col justify-center items-center"
+    >
+      <Card className="max-w-4xl mx-auto">
+        <Text className="text-6xl font-bold text-center mb-10">
+          Weather App
+        </Text>
+        <Subtitle className="text-xl text-center">Powered by OpenAi</Subtitle>
 
-    <Divider className="my-10"/>
-  
-  <Card className="bg-gradient-to-br from-[#394f68] to-[#18387e]">
-   <CityPicker />
+        <Divider className="my-10" />
 
-  </Card>
-    </Card>
+        <Card className="bg-gradient-to-br from-[#394f68] to-[#18387e]">
+          <CityPicker />
+        </Card>
+      </Card>
     </div>
-  )
+  );
 }
