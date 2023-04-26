cdeb7df926a4b3e274c93b75f91645224cc5082a
mwendaB <brianmwenda255@gmail.com>
2023-04-26 15:28:10 +0300

    pulling all the counties inside my app

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,7 +1,23 @@
+ "use client";
+
 import { Country, City } from "country-state-city"
+import Select from  "react-select";
+
+const options = Country.getAllCountries().map((country) =>  ({
+    value:{
+        latitude: country.latitude,
+        longitude: country.longitude,
+        isoCode: country.isoCode
+    },
+    label: country.name,
+}));
+
+
 function CityPicker() {
   return (
-    <div>CityPicker</div>
+    <div>
+        <Select options={options}/>
+    </div>
   )
 }
 
